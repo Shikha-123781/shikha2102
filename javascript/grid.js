e1b1a9ce2a0427eda4bdcf9bfc1d7c12ccae75f3
Shikha Shrivastav <sshrivastav@grepruby.io>
2020-01-22 20:04:57 +0530

    gridplay

--- javascript/grid.js
+++ javascript/grid.js
@@ -1,31 +1,26 @@
-function gridColour(i)
-{
-  
+function gridColour(i) {
   var k,result;
   var p=document.getElementById(i);
   var q= document.getElementsByClassName(
     "colour");
-  for(k=0;k<q.length;k++)
-    {
-      if(q[k].checked)
-       { result=q[k].value;
+  for(k=0;k<q.length;k++) {
+      if(q[k].checked) {
+       result=q[k].value;
         break;
        }
-    }
+  }
 p.style.backgroundColor=result;
 }
-function reset()
-{
+function reset() {
   var i;
-  var a=     document.getElementsByClassName(
+  var a = document.getElementsByClassName(
     "aa");
-  var b=     document.getElementsByClassName(
+  var b = document.getElementsByClassName(
     "bb");
-  var c=     document.getElementsByClassName(
+  var c = document.getElementsByClassName(
     "cc");
-  for(i=0;i<3;i++)
-    {
+  for(i=0;i<3;i++) {
       a[i].style.backgroundColor="#aaa";  b[i].style.backgroundColor="#aaa";c[i].style.backgroundColor="#aaa";
-    }   
+  }   
 }
  
