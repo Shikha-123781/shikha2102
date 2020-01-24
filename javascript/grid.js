b73cd62f0c9338fc9edb6c228c079f77645d656a
Shikha Shrivastav <sshrivastav@grepruby.io>
2020-01-24 19:50:56 +0530

    add suffle() function

--- javascript/grid.js
+++ javascript/grid.js
@@ -1,26 +1,54 @@
 function gridColour(i) {
   var k,result;
   var p=document.getElementById(i);
-  var q= document.getElementsByClassName(
-    "colour");
+  var q= document.getElementsByClassName("colour");
   for(k=0;k<q.length;k++) {
-      if(q[k].checked) {
-       result=q[k].value;
-        break;
-       }
+    if(q[k].checked) {
+      result=q[k].value;
+      break;
+    }
   }
-p.style.backgroundColor=result;
+  p.style.backgroundColor=result;
 }
 function reset() {
   var i;
-  var a = document.getElementsByClassName(
-    "aa");
-  var b = document.getElementsByClassName(
-    "bb");
-  var c = document.getElementsByClassName(
-    "cc");
+  var arr =['a','b','c','d','e','f','g','h','i'];
+  var a = document.getElementsByClassName("aa");
+  var b = document.getElementsByClassName("bb");
+  var c=  document.getElementsByClassName("cc");
   for(i=0;i<3;i++) {
-      a[i].style.backgroundColor="#aaa";  b[i].style.backgroundColor="#aaa";c[i].style.backgroundColor="#aaa";
-  }   
+    a[i].style.backgroundColor="#aaa";  b[i].style.backgroundColor="#aaa";c[i].style.backgroundColor="#aaa";
+  } 
+  for(i=0;i<9;i++) {
+    var x = document.getElementById(arr[i]);
+    x.innerHTML=i+1;
+  }
 }
+function suffle() {
+  var i;
+  var arr=[1,2,3,4,5,6,7,8,9];
+  var a=["a",'b','c','d','e','f','g','h','i'];
+  var l=[];
+  for(i=0;i<9;i++) {
+    do{
+    l[i]=arr[Math.floor(Math.random()*9)];
+    } while(check(l[i],l,i)==1); 
+  var x = document.getElementById(a[i]);
+  x.innerHTML=l[i];
+  }
+}
+function check(value,l,i) {
+  var j;
+  flag=0;
+  for(j=i-1;j>=0;j--) {
+    if(l[i]==l[j]) {
+      flag=1;
+      break;
+     }
+  }
+  return flag;
+}
+
+
+
  
