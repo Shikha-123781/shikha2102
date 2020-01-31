85e764e6411f56b30defaf41ca0aaab7796d3fc8
Shikha Shrivastav <sshrivastav@grepruby.io>
2020-01-31 21:11:34 +0530

    added all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ReadComponent } from './read/read.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent,
+    ReadComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
