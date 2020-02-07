1deb4a63e44202a5ec388913d1761e027a8853c6
Shikha Shrivastav <sshrivastav@grepruby.io>
2020-02-07 20:24:09 +0530

    templateDrivenForm

---
+++ templateDriven/src/app/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
---
+++ templateDriven/src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  
+  contact = new Contact();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSubmit(){
+    alert("data is inserted");
+  }
+
+}
